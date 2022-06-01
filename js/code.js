a3f24e208ebb2cc3996717943f769259abe89fb4
matsanchez <smatu22@gmail.com>
2022-06-01 20:07:45 -0300

    Corrigiendo sentencia for

--- js/code.js
+++ js/code.js
@@ -1,34 +1,33 @@
 alert("Bienvenidos al Primer Desafio 'MiniCalculadora'")
-alert("¿Que operacion quieres realizar?")
-for (let operation = 0; operation <=4; operation=0) {
-    let operation = prompt("( 1 ) SUMA - ( 2 ) RESTA - ( 3 ) MULTIPLICACION - ( 4 ) DIVISION");
+alert("Vamos a realizar las 4 Operaciones Basicas de Matematicas")
+
+for (let operation = 0; operation < 5; operation++) {
+    
     if(operation == 1){
-        let num1 = prompt("Ingrese el primer numero para sumar");
+        let num1 = prompt("Primero vamos a sumar: Ingrese el primer numero");
         let num2 = prompt("Ingrese el segundo numero para sumar");
         let result = (parseInt(num1) + parseInt(num2));
         alert("El resultado de la Suma es: " + result);
     }
     else if(operation == 2){
-        let num1 = prompt("Ingrese el primer numero para restar");
+        let num1 = prompt("Ahora vamos a Restar: Ingrese el primer numero");
         let num2 = prompt("Ingrese el segundo numero para restar");
         let result = (parseInt(num1) - parseInt(num2));
         alert("El resultado de la Resta es: " + result);
     }
     else if(operation == 3){
-        let num1 = prompt("Ingrese el primer numero para Multiplicar");
+        let num1 = prompt("Ahora toca Multiplicar: Ingrese el primer numero");
         let num2 = prompt("Ingrese el segundo numero para Multiplicar");
         let result = (parseInt(num1) * parseInt(num2));
         alert("El resultado de la Multiplicacion es: " + result);
     }
     else if(operation == 4){
-        let num1 = prompt("Ingrese el primer numero para Dividir");
+        let num1 = prompt("Y por ultimo vamos a Dividir: Ingrese el primer numero");
         let num2 = prompt("Ingrese el segundo numero para Dividir");
         let result = (parseInt(num1) / parseInt(num2));
         alert("El resultado de la Division es: " + result);
     }
-    else{
-        alert("No existe ninguna operacion con ese numero");
-    }
-}  
+}
+alert("Gracias por usar la calculadora, si necesitas volver a usarla oprimi F5, hasta la proxima!")  
 
 
